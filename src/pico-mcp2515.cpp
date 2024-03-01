0be5342b1faf9ba9a010b6a2d6570cf8e8aa94ce
eepykami <155262929+eepykami@users.noreply.github.com>
2024-03-01 01:05:23 +0000

    Add code for receiving inbound serial messages

--- src/pico-mcp2515.cpp
+++ src/pico-mcp2515.cpp
@@ -2,8 +2,37 @@
 #include "pico/stdlib.h"
 #include "mcp2515/mcp2515.h"
 
+// Buffer size and values for serial input
+#define ENDSTDIN 255
+#define CR 13
+#define SERIAL_DATA_BUFFER_SIZE 500
+
+// Variables for serial input
+int string_buffer_offset = 0; 
+char string_buffer[SERIAL_DATA_BUFFER_SIZE]; 
+char input_character;
+
+// Variables for MCP2515
 MCP2515 can0;
 struct can_frame rx;
+
+void processInboundSerialMessages() {
+    input_character = getchar_timeout_us(0);
+    while (input_character != ENDSTDIN)
+    {   
+        string_buffer[string_buffer_offset++] = input_character;
+        if (input_character == CR)
+        {
+            string_buffer[string_buffer_offset] = 0;
+            // do something with message in string buffer here. for now we printf it
+            // TODO: plan is for a can packet to come in via serial. decode it from the string buffer, and send it via CAN.
+            printf("%s\n", string_buffer);
+            string_buffer_offset = 0;
+            break;
+        }
+        input_character = getchar_timeout_us(0);
+    }
+}
 
 int main() {
     stdio_init_all();
@@ -34,6 +63,7 @@ int main() {
     // Listen loop
     printf("Listening for GMLAN messages ...\n");
     while(true) {
+        processInboundSerialMessages();
         if(can0.readMessage(&rx) == MCP2515::ERROR_OK) {
             printf("New frame from ID: %10x\n", rx.can_id);
         }
